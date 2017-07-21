8024557aefa6ea13b218b4f100fc2901fe89261c
ysugimoto <sugimoto@wnotes.net>
2017-07-21 19:08:21 +0900

    [WIP] implement actions

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"strings"
 	"time"
@@ -96,6 +97,15 @@ func main() {
 	}
 
 	service = s3.New(sess, conf)
+	bucket, err := chooseBuckets()
+	if err != nil {
+		return
+	}
+	fmt.Printf("Retriving %s bucket...\n", bucket)
+	chooseObjects(bucket, []string{})
+}
+
+func chooseBuckets() (string, error) {
 	result, err := service.ListBuckets(&s3.ListBucketsInput{})
 	if err != nil {
 		panic(err)
@@ -108,10 +118,9 @@ func main() {
 
 	selected, err := p.Transform(buckets)
 	if err != nil {
-		return
+		return "", err
 	}
-	fmt.Printf("Retirving %s bucket...\n", selected)
-	chooseObjects(selected, []string{})
+	return selected, nil
 }
 
 // Choose from object list
@@ -134,29 +143,96 @@ func chooseObjects(bucket string, prefix []string) {
 	if selected == "../" {
 		if len(prefix) > 0 {
 			prefix = prefix[0 : len(prefix)-1]
+		} else {
+			b, err := chooseBuckets()
+			if err != nil {
+				return
+			}
+			bucket = b
+			prefix = []string{}
 		}
 	} else if strings.HasSuffix(selected, "/") {
-		selected = strings.TrimRight(selected[20:], "/")
+		selected = strings.TrimRight(selected[32:], "/")
 		prefix = append(prefix, selected)
 	} else {
-		return
+		selected = strings.TrimRight(selected[32:], "/")
+		if isEnd := displayObjectActions(bucket, prefix, selected); isEnd {
+			return
+		}
 	}
 	chooseObjects(bucket, prefix)
 }
 
+// Display object info for slected object
+func displayObjectActions(bucket string, prefix []string, object string) bool {
+	input := &s3.GetObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(fmt.Sprintf("%s/%s", strings.Join(prefix, "/"), object)),
+	}
+	result, err := service.GetObject(input)
+	if err != nil {
+		panic(err)
+	}
+	jst, _ := utcToJst(*result.LastModified)
+	fmt.Println(strings.Repeat("=", 60))
+	if len(prefix) > 0 {
+		fmt.Printf("%-16s: s3://%s/%s\n", "Location", bucket, object)
+	} else {
+		fmt.Printf("%-16s: s3://%s/%s/%s\n", "Location", bucket, strings.Join(prefix, "/"), object)
+	}
+	fmt.Printf("%-16s: %s\n", "ContentType", *result.ContentType)
+	fmt.Printf("%-16s: %d\n", "FileSize", *result.ContentLength)
+	fmt.Printf("%-16s: %s\n", "LastModified", jst)
+	fmt.Println("")
+	return acceptAction(result)
+}
+
+func acceptAction(result *s3.GetObjectOutput) bool {
+	ct := *result.ContentType
+	fmt.Print("Actions? [d:Download")
+	if strings.HasPrefix(ct, "text/") || strings.HasPrefix(ct, "application/") {
+		fmt.Print(", v:View")
+	}
+	fmt.Print(", b:Back to list]: ")
+	var cmd string
+	fmt.Scanf("%s", &cmd)
+	switch cmd {
+	case "d":
+		fmt.Println("Download")
+		return true
+	case "v":
+		if strings.HasPrefix(ct, "text/") || strings.HasPrefix(ct, "application/") {
+			if buf, err := ioutil.ReadAll(result.Body); err != nil {
+				fmt.Println(err)
+			} else {
+				fmt.Println(string(buf))
+			}
+			return false
+		} else {
+			fmt.Println("Invalid access v action")
+		}
+		return true
+	case "b":
+		return false
+	default:
+		fmt.Println("Invalid command")
+		return acceptAction(result)
+	}
+}
+
 // Fromat object list from S3 response
 func formatObjectList(result []*s3.Object, prefix []string) (objects []string) {
-	if len(prefix) > 0 {
-		objects = append(objects, "../")
-	}
+	objects = append(objects, "../")
 	unique := map[string]struct{}{}
 	var rep string
 	if len(prefix) > 0 {
 		rep = strings.Join(prefix, "/") + "/"
 	}
 	for i := 0; i < len(result); i++ {
+		isDir := false
 		key := *result[i].Key
 		lastModified := *result[i].LastModified
+		size := *result[i].Size
 		key = strings.Replace(key, rep, "", 1)
 		if strings.Contains(key, "/") {
 			spl := strings.Split(key, "/")
@@ -165,13 +241,18 @@ func formatObjectList(result []*s3.Object, prefix []string) (objects []string) {
 			}
 			unique[spl[0]] = struct{}{}
 			key = spl[0] + "/"
+			isDir = true
 		}
 		jst, err := utcToJst(lastModified)
 		if err != nil {
 			fmt.Println(err)
 			continue
 		}
-		objects = append(objects, fmt.Sprintf("%s %s", jst, key))
+		if isDir {
+			objects = append(objects, fmt.Sprintf("%s %10s  %s", jst, "-", key))
+		} else {
+			objects = append(objects, fmt.Sprintf("%s %10d  %s", jst, size, key))
+		}
 	}
 	return
 }
